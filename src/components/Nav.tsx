a8dc92a053ba3b7d1ee3a0604fd45123d1a5a369
Cohen Erickson <cohenerickson@gmail.com>
2022-07-05 15:18:59 -0500

    Scrollbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ import NavbarButton from "./NavbarButton";
 
 export default function Nav (props) {
   return (
-    <nav class="bg-background rounded w-full h-10 mb-2">
+    <nav class="bg-background rounded w-full h-16 mb-2">
       <div class="w-full h-full flex justify-center items-center">
         <NavbarButton href="/#home">Home</NavbarButton>
         <NavbarButton href="/#projects">Projects</NavbarButton>
