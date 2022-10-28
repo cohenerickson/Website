9f3e215b5afaf7a022c80f647baa84e3305c1af4
Cohen Erickson <cohenerickson@gmail.com>
2022-10-28 18:41:55 +0000

    Contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,15 +1,33 @@
-import { JSX, onMount } from "solid-js";
-import FadeIn from "./FadeIn";
+import { JSX } from "solid-js";
+import FadeIn from "~/components/FadeIn";
 
 export default function About(): JSX.Element {
   return (
-    <FadeIn
-      class="flex flex-col w-full justify-center gap-5 px-32 h-[calc(100vh-96px)]"
-      id="contact"
-    >
-      <div class="flex items-center gap-5">
-        <span class="text-3xl text-accent">Get in touch</span>
-        <div class="h-[1px] bg-textPrimary w-1/2"></div>
+    <FadeIn class="px-8 md:px-32 h-[calc(100vh-96px)]">
+      <div
+        id="contact"
+        class="flex flex-col w-full h-full justify-center gap-5"
+      >
+        <div>
+          <span class="text-3xl text-accent">
+            Looking to bring your idea alive?
+          </span>
+          <h1 class="text-5xl font-bold">Get in touch</h1>
+        </div>
+        <p class="text-lg">
+          I'm currently looking for new opportunities. My inbox is always open.
+          Whether you have a question or just want to say hi, I'll try my best
+          to get back to you!
+        </p>
+        <div>
+          <a
+            href="mailto:inquries@cohenerickson.com"
+            target="_blank"
+            class="py-2 px-3 bg-accent rounded-lg transition-all"
+          >
+            Send me an email!
+          </a>
+        </div>
       </div>
     </FadeIn>
   );
--- src/components/Experience/Item.tsx
+++ src/components/Experience/Item.tsx
@@ -9,7 +9,14 @@ export default function Item(props: ItemProps): JSX.Element {
   return (
     <div class="flex gap-5">
       <span class="text-accent">-</span>
-      <a href={props.link} target="_blank" class="text-textPrimary border-expand">{props.name}</a>
+      <a
+        href={props.link}
+        target="_blank"
+        title={props.name}
+        class="text-textPrimary border-expand"
+      >
+        {props.name}
+      </a>
     </div>
   );
 }
