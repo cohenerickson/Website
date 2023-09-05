44914e88c21d41c705c1dd847609814102f5e3cc
Cohen Erickson <cohenerickson@gmail.com>
2023-09-05 18:04:02 -0500

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -14,13 +14,13 @@ export default function About(): JSX.Element {
         <h1 class="text-5xl font-bold">Get in touch</h1>
       </div>
       <p class="text-lg mb-2">
-        I'm currently looking for new opportunities. My inbox is always open.
+        I'm not currently looking for new opportunities however my inbox is always open.
         Whether you have a question or just want to say hi, I'll try my best to
         get back to you!
       </p>
       <div>
         <a
-          href="mailto:inquries@cohenerickson.com"
+          href="mailto:contact@cohenerickson.com"
           target="_blank"
           class="py-3 px-4 bg-accent rounded-lg transition-all text-lg shadow-lg hover:shadow-2xl"
         >
